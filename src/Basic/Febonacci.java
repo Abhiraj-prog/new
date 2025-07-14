be98eb7f0bfd12af85fcd0c835e02c8f5f31f4c8
Abhiraj-prog <abhi2feb012@gmail.com>
2025-07-14 19:13:05 +0530

    prime.java

---
+++ src/Basic/Febonacci.java
@@ -0,0 +1,15 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class Febonacci {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        int a=0;
+        int b=1;
+
+        for ()
+
+    }
+}
