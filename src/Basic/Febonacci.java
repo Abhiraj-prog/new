9318ca99ecc5ffbd4c199f483827efe4c8c76d55
Abhiraj-prog <abhi2feb012@gmail.com>
2025-07-14 19:22:45 +0530

    prime.java

--- src/Basic/Febonacci.java
+++ src/Basic/Febonacci.java
@@ -6,10 +6,14 @@ public class Febonacci {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n =sc.nextInt();
-        int a=0;
+        int a =0;
         int b=1;
-
-        for ()
+        for (int i=0;i<n;i++){
+            System.out.println(a);
+            int c =a+b;
+            a=b;
+            b=c;
+        }
 
     }
 }
